e2eb7b8e19308788b1fa4c0a3abfb10d8b2378bc
Hugo Cortez <hugocortezprog@gmail.com>
2024-12-04 14:03:53 -0300

    menu

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isMenuOpen: boolean = false;
+
+  menuItens = [
+    {
+      link: "/paises",
+      iconUrl: "/assets/icons/icon-country-search.svg",
+      alt: "buscar pais",
+      name: "Países"
+    }
+  ]
+}
