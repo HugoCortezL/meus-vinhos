65850b0683dbec3ab1b0727774e8a4bebba64a83
Hugo Cortez <hugocortezprog@gmail.com>
2024-12-04 12:25:25 -0300

    paises pronto

--- src/app/pages/country-list/country-list.component.ts
+++ src/app/pages/country-list/country-list.component.ts
@@ -3,15 +3,20 @@ import { Country } from '../../core/models/Country.interface';
 import { CountryService } from '../../core/services/country.service';
 import { CommonModule } from '@angular/common';
 import { CountryCardComponent } from '../../shared/components/country-card/country-card.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-country-list',
-  imports: [CommonModule, CountryCardComponent],
+  imports: [CommonModule, ReactiveFormsModule, CountryCardComponent],
   templateUrl: './country-list.component.html',
   styleUrl: './country-list.component.scss'
 })
 export class CountryListComponent {
   countries: Country[] = [];
+  searchControl: FormControl = new FormControl('');
+  searchText: string = '';
+  sortOrder: string = 'nameAsc';
 
   constructor(private countryService: CountryService) { }
 
@@ -20,5 +25,37 @@ export class CountryListComponent {
     this.countryService.getAllCountries().subscribe((data) => {
       this.countries = data;
     });
+
+    this.searchControl.valueChanges.pipe(
+      debounceTime(200),
+      distinctUntilChanged()
+    ).subscribe(value => {
+      this.searchText = value;
+      console.log(value)
+    });
+  }
+
+  filteredCountries() {
+    let filtered = this.countries.filter(country =>
+      country.name.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+
+    // Ordena os países com base na seleção de ordenação
+    return this.sortCountries(filtered);
+  }
+
+  sortCountries(filteredCountries: Country[]): Country[] {
+    switch (this.sortOrder) {
+      case 'nameAsc':
+        return filteredCountries.sort((a, b) => a.name.localeCompare(b.name)); // Ordena pelo nome em ordem crescente
+      case 'nameDesc':
+        return filteredCountries.sort((a, b) => b.name.localeCompare(a.name)); // Ordena pelo nome em ordem decrescente
+      default:
+        return filteredCountries;  // Se nenhuma ordem foi definida, retorna a lista sem ordenar
+    }
+  }
+
+  onSortCountryChange(event: any): void {
+    this.sortOrder = event.target.value;
   }
 }
